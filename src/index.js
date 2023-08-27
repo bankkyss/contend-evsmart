3eb91d88a0c5c9a2350eca28834c230eeec6738e
supanus.s <supanus.s@securitypitch.com>
2023-08-28 02:54:55 +0700

    edit for git

--- src/index.js
+++ src/index.js
@@ -1,7 +1,64 @@
+// import React from "react";
+// import { createRoot } from "react-dom/client";
+// import {
+//   createBrowserRouter,
+//   RouterProvider,
+//   // Route,
+//   // Link,
+//   Outlet,
+// } from "react-router-dom";
+// import Navbar from "./components/Navbar";
+// import "./App.css";
+// import Home from "./routes/Home";
+// import Upload from "./routes/upload";
+// import Route from "./routes/route";
+// import Freeform from "./routes/FreeForm";
+// import ErrorPage from "./routes/ErrorPage";
+
+
+// const AppLayout = () => {
+//   return (
+//     <>
+//       <Navbar />
+//       <Outlet />
+//     </>
+//   );
+// };
+
+// const router = createBrowserRouter([
+//   {
+//     element: <AppLayout />,
+//     errorElement: <ErrorPage />,
+//     children: [
+//       {
+//         path: "/",
+//         element: <Home />,
+//       },
+//       {
+//         path: "upload",
+//         element: <Upload />,
+//       },
+//       {
+//         path: "route",
+//         element: <Route />,
+//       },
+//       {
+//         path:"freeform",
+//         element : <Freeform />
+//       }
+//     ],
+//   },
+// ]);
+
+// createRoot(document.getElementById("root")).render(
+//   <RouterProvider router={router} />
+// );
+
+
 import React from "react";
 import { createRoot } from "react-dom/client";
 import {
-  createBrowserRouter,
+  createHashRouter,
   RouterProvider,
   // Route,
   // Link,
@@ -25,7 +82,7 @@ const AppLayout = () => {
   );
 };
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     element: <AppLayout />,
     errorElement: <ErrorPage />,
